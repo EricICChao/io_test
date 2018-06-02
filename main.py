595355f4b4fadfa8842dd4068c010efa9bb87767
EricICChao <39307195+EricICChao@users.noreply.github.com>
2018-06-02 10:47:02 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+user_id = "1234567"
+user_name = "eric_test"
+
+#寫入檔案
+
+f1 = open('test.txt', 'at', encoding='utf-8' )
+send_message = input('請輸入:\n')  #輸出的文字需要換行
+#message_record = user_id + ":" + send_message
+f1.write(user_id + ":" + send_message + '\n')
+f1.close()
+
+
+
+#檔案中文字一行一行列出並標示行數
+#a = 0
+
+f = open("test.txt", "rt")
+#text_string =str(str(a) + ":" + f.readline())  #確保數字轉字串與串接字串
+f.readline()
+#a = a+1
+f.close()
+
+print("===============")
+
+#列印出目前檔案中的全部文字
+f = open("test.txt", "rt")
+print(f.read())
+f.close()
+
+
+
+"""
+#json版本
+
+
+
+
+#csv版本
+"""
